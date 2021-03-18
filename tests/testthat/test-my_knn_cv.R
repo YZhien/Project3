aa9708e8f8d81d0ee8b6405a84b86184fde81b3e
Zhien Yu <selina011117@gmail.com>
2021-03-19 02:44:16 +0800

    test

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -5,6 +5,10 @@ test_that("my_knn_cv works", {
 test_that("non-suitable input for parameters throws error", {
   expect_error(my_knn_cv(my_gapminder[4],"a",3,3))
   expect_error(my_knn_cv(my_gapminder[4],"a","t",3))
+  expect_error(my_knn_cv(my_gapminder[4],"a","t",'yu'))
+  expect_error(my_knn_cv(777,"a","t",'yu'))
+  expect_error(my_knn_cv(my_gapminder[4],my_gapminder[4],"t",'yu'))
+  expect_error(my_knn_cv(NA,my_gapminder[4],"t",'yu'))
 })
 
 
